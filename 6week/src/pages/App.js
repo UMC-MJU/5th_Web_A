8b62b58c75e657cbb99a3433c47182543227fdb1
권건율 <cjdak0907@naver.com>
2023-11-08 15:34:32 +0900

    6주차 과제

---
+++ 6week/src/pages/App.js
@@ -0,0 +1,21 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Movies from "../Components/Movies/Movies";
+import Home from "../Components/Home/Home";
+import MoviesDetail from "../Components/Movies/MoviesDetail";
+import NotFound from "../Components/NotFound";
+function App() {
+  return (
+    <div className="root-wrap">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/"element={<Home/>}/>
+            <Route path="/movie" element={<Movies/>}/>
+            <Route path="/movie/:title" element={<MoviesDetail/>}/>
+            <Route path="*" element={<NotFound/>}/>
+          </Routes>
+        </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
